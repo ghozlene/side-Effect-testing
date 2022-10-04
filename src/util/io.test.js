796b6d4341d0cb57cc20ef37adb36909a45a4915
Achref <medachrefg@gmail.com>
2022-10-04 14:19:56 +0100

    custom mocking logic

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -3,14 +3,22 @@ import writeData from './io';
 import { promises as fs } from 'fs';
 
 vi.mock('fs');
-describe('writeData', () => {
+vi.mock('path', () => {
+	return {
+		default: {
+			join: (...args) => {
+				return args[args.length - 1];
+			},
+		},
+	};
+});
+
+describe('writeData()', () => {
 	it('should execute the writeFile method', () => {
 		const testData = 'Achref';
 		const fileName = 'test.txt';
 		writeData(testData, fileName);
-		expect(fs.writeFile).toBeCalled();
+		expect(fs.writeFile).toBeCalledWith(fileName, testData);
 		//return expect(writeData(testData, fileName)).resolves.toBeUndefined();
 	});
-
-	it('it should writing on the file when path & filename provided ', () => {});
 });
