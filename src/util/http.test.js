5ebea8196dfb03c02ac7af6aa9ab4d460dd5da72
Achref <medachrefg@gmail.com>
2022-10-05 02:00:42 +0100

    testing specific mocking logic

--- src/util/http.test.js
+++ src/util/http.test.js
@@ -7,6 +7,9 @@ const testResponseData = {
 
 const testFn = vi.fn((url, options) => {
 	return new Promise((reslove, reject) => {
+		if (typeof options.body !== 'string') {
+			return reject('not a string');
+		}
 		const testResp = {
 			ok: true,
 			json() {
@@ -24,5 +27,17 @@ describe('sendDataRequest()', () => {
 	it('should return any available res data ', () => {
 		const testData = { key: 'test' };
 		return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+	});
+
+	it('should convert the provided data to JSON before sending Request ', async () => {
+		const testData = { key: 'test' };
+		let errorMessage;
+		try {
+			await sendDataRequest(testData);
+		} catch (err) {
+			errorMessage = err;
+		}
+
+		expect(errorMessage).not.toBe('not a string');
 	});
 });
